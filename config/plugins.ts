515f7fd84717aabbde8acd0255e5be72b1802139
mrebukks <obiebukaugonna@gmail.com>
2026-01-13 13:55:02 +0100

    added rest cache plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+module.exports = {
+  'rest-cache': {
+    config: {
+      provider: {
+        name: 'memory', // No extra setup needed for this
+      },
+      strategy: {
+        contentTypes: [
+          'api::article.article', // Replace with your actual Content Type UID
+        ],
+      },
+    },
+  },
+};
